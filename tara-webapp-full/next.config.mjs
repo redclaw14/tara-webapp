2798d559ba8f54ad9e9bc597b6841d4e3273e349
redclaw14 <Niklaslemkee@gmail.com>
2025-08-08 17:02:21 +0700

    Update next.config.mjs

--- tara-webapp-full/next.config.mjs
+++ tara-webapp-full/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  experimental: { serverActions: { bodySizeLimit: '2mb' } },
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'images.unsplash.com' },
+      { protocol: 'https', hostname: 'images.pexels.com' },
+    ],
+  },
 };
 export default nextConfig;
